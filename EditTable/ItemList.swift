f29fde4678339fa021a7ebbf2fb448d8f157ae05
isaakwheeler <isaak.wheeler@hotmail.com>
2022-10-07 17:18:26 -0700

    mod item names

--- EditTable/ItemList.swift
+++ EditTable/ItemList.swift
@@ -11,8 +11,8 @@ struct ItemList{
     var listOfItems: [Item] = []
     init(){
         listOfItems.append(Item(Name: "Soup", Cost: 2.50))
-        listOfItems.append(Item(Name: "Soup", Cost: 2.50))
-        listOfItems.append(Item(Name: "Soup", Cost: 2.50))
+        listOfItems.append(Item(Name: "Meat", Cost: 4.50))
+        listOfItems.append(Item(Name: "Carrot", Cost: 2.10))
     }
     mutating func add(newItem: Item){
         listOfItems.append(newItem)
